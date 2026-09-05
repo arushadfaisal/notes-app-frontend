ec883d35f2686860cbeaee800d9f55f6cc7a856f
Arushad Faisal <arushadfaisal@gmail.com>
2026-09-05 14:13:45 +0500

    create edit functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,17 @@
 import { useEffect, useState } from 'react'
 import { MdDelete } from 'react-icons/md'
+import { MdEdit } from 'react-icons/md'
 import './App.css'
-
-function Note({ id, title, content, onDelete }){
-  return(
-    <div className="flex flex-col bg-slate-700 p-4 pt-2 shadow-sm min-h-30">
-      <div className="flex flex-row width-max place-content-between">
-        <div className="p-2 min-h-10 text-start font-bold text-slate-200 text-lg">{title}</div>
-        <button className="p-2 my-2 text-ls bg-red-900 rounded text-slate-100" onClick={() => onDelete(id)}>
-                <MdDelete />
-        </button>
-      </div>
-      <div className="p-2 min-h-20 text-start text-slate-300 bg-slate-800/50">{content}</div>
-    </div>
-    );
-  }
 
 function App() {
   const [list, setList] = useState([]);
   const [title, setTitle] = useState('');
   const [content, setContent] = useState('');
+  const [editId, setEditId] = useState(null);
+
+  //Button Control
+  const [addButton, setAddButton] = useState('Add');
+  const [resetButton, setResetButton] = useState('Reset');
 
   useEffect(() => {
     const fetchNotes = async () => {
@@ -37,38 +29,80 @@ function App() {
     fetchNotes();
   }, []);
 
-  const addNote = async (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
 
-    if (title.trim() === '' || content.trim() === ''){
-      return (console.error("Please enter all information"));
-    }
+    if(editId){
 
-    try {
-      const response = await fetch('http://127.0.0.1:8000/api/notes', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ title,content }),
-      });
+      if (title.trim() === '' || content.trim() === ''){
+        return (console.error("Please enter all information"));
+      }else{
+        try{
+          const response = await fetch (`http://127.0.0.1:8000/api/notes/${editId}`, {
+            method: 'PUT',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ title, content })
+          });
 
-      const newNote = await response.json();
+          const updatedNote = await response.json();
 
-      setList([...list, newNote]);
+          setList(previousList => 
+            previousList.map(note =>
+              note.id === editId ? updatedNote : note
+            )
+          );
 
-      setTitle('');
-      setContent('');
-    }
-    catch (error) {
-      console.error("Error adding new note: ", error);
+        }catch (error){
+          console.error("Error editing note: ", error);
+        }
+      }
+
+
+
+      handleReset();
+      return console.log("This worked");
+    }else{
+      if (title.trim() === '' || content.trim() === ''){
+        return (console.error("Please enter all information"));
+      }
+
+      try {
+        const response = await fetch('http://127.0.0.1:8000/api/notes', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ title,content }),
+        });
+
+        const newNote = await response.json();
+
+        setList([...list, newNote]);
+
+        handleReset();
+      }
+      catch (error) {
+        console.error("Error adding new note: ", error);
+      }
     }
   }
 
   const handleReset = () => {
     setTitle('');
     setContent('');
+    setAddButton('Add');
+    setResetButton('Reset');
+    setEditId(null);
   };
+
+  const setEdit = (title, content) => {
+    setAddButton('Confirm');
+    setResetButton('Cancel');
+    setTitle(title);
+    setContent(content);
+  }
 
   const handleDelete = async (id) => {
     try{
@@ -86,6 +120,25 @@ function App() {
     }
   }
 
+  function Note({ id, title, content, onDelete }){
+    return(
+      <div className="flex flex-col bg-slate-700 p-4 pt-2 shadow-sm min-h-30">
+        <div className="flex flex-row width-max place-content-between">
+          <div className="p-2 min-h-10 text-start font-bold text-slate-200 text-lg">{title}</div>
+          <div className="flex flex-row gap-2">
+            <button className="p-2 my-2 text-ls bg-blue-900/85 rounded text-slate-100" onClick={() => {setEditId(id); setEdit(title, content);}}>
+                    <MdEdit />
+            </button>
+            <button className="p-2 my-2 text-ls bg-red-900/85 rounded text-slate-100" onClick={() => onDelete(id)}>
+                    <MdDelete />
+            </button>
+          </div>
+        </div>
+        <div className="p-2 min-h-20 text-start text-slate-300 bg-slate-800/50">{content}</div>
+      </div>
+    );
+  }
+
   return (
     <>
       <section id="header">
@@ -96,7 +149,7 @@ function App() {
       </section>
 
       <section id="body">
-        <form onSubmit={addNote} onReset={handleReset}
+        <form onSubmit={handleSubmit} onReset={handleReset}
         className="addNote flex flex-row w-full h-auto mx-auto my-4 mb-10 max-w-9/10 md:max-w-8/10 
                     justify-center items-center bg-slate-700/10">
           <div className="flex flex-col w-full h-auto items-center gap-2 p-3">
@@ -122,8 +175,8 @@ function App() {
                 className="w-full min-h-32 h-auto p-4 bg-slate-700/30 align-top resize-y"/>
             </label>
             <div id="addNoteButtons" className='flex w-full flex-row gap-4 justify-between'>
-                <button className='px-3 py-1  bg-indigo-300/80 text-indigo-900' type="reset">Reset</button>
-                <button className='px-3 py-1 font-bold bg-indigo-900 text-indigo-200' type="submit">Add</button>
+                <button className='px-3 py-1  bg-indigo-300/80 text-indigo-900' type="reset">{resetButton}</button>
+                <button className='px-3 py-1 font-bold bg-indigo-900 text-indigo-200' type="submit">{addButton}</button>
             </div>
           </div>
         </form>
@@ -131,7 +184,7 @@ function App() {
 
       <section id="notes">
         <div className="grid grid-cols-1 gap-6 p-4">
-                {Array.isArray(list) && list.map((note, index) => (
+                {Array.isArray(list) && list.map((note) => (
                   <Note key={note.id} id={note.id} title={note.title} content={note.content} onDelete={handleDelete} />
                 ))}
         </div>
